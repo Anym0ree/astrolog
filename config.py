acec4c7f79d5a71440bca66c2efcdff1af1c2c8b
Anym0ree <matvejknazev091@gmail.com>
2026-05-25 21:23:01 +0300

    Create config.py

---
+++ config.py
@@ -0,0 +1,12 @@
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
+
+BOT_TOKEN = os.getenv("BOT_TOKEN")
+OPENAI_API_KEY = os.getenv("OPENAI_API_KEY")
+GROUP_CHAT_ID = int(os.getenv("GROUP_CHAT_ID"))
+TIMEZONE = os.getenv("MOSCOW_TIMEZONE", "Europe/Moscow")
+MORNING_HOUR = int(os.getenv("MORNING_HOUR", 9))
+MORNING_MINUTE = int(os.getenv("MORNING_MINUTE", 0))
+OPENAI_MODEL = os.getenv("OPENAI_MODEL", "gpt-3.5-turbo")
