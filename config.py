129985b28fae500aafc106f9341aa5960695c818
Anym0ree <matvejknazev091@gmail.com>
2026-05-25 22:08:37 +0300

    Update GROUP_CHAT_ID handling in config.py
    Modify GROUP_CHAT_ID assignment to handle None case.

--- config.py
+++ config.py
@@ -5,7 +5,11 @@ load_dotenv()
 
 BOT_TOKEN = os.getenv("BOT_TOKEN")
 OPENAI_API_KEY = os.getenv("OPENAI_API_KEY")
-GROUP_CHAT_ID = int(os.getenv("GROUP_CHAT_ID"))
+GROUP_CHAT_ID = os.getenv("GROUP_CHAT_ID")
+if GROUP_CHAT_ID:
+    GROUP_CHAT_ID = int(GROUP_CHAT_ID)
+else:
+    GROUP_CHAT_ID = None
 TIMEZONE = os.getenv("MOSCOW_TIMEZONE", "Europe/Moscow")
 MORNING_HOUR = int(os.getenv("MORNING_HOUR", 9))
 MORNING_MINUTE = int(os.getenv("MORNING_MINUTE", 0))
