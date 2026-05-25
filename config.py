0ceb2b2174c4e5e0c822398d080851bc84965a27
Anym0ree <matvejknazev091@gmail.com>
2026-05-25 22:24:51 +0300

    Update config.py

--- config.py
+++ config.py
@@ -5,11 +5,14 @@ load_dotenv()
 
 BOT_TOKEN = os.getenv("BOT_TOKEN")
 OPENAI_API_KEY = os.getenv("OPENAI_API_KEY")
-GROUP_CHAT_ID = os.getenv("GROUP_CHAT_ID")
-if GROUP_CHAT_ID:
-    GROUP_CHAT_ID = int(GROUP_CHAT_ID)
+
+# Безопасное получение GROUP_CHAT_ID (может быть строка "None", пусто или отсутствовать)
+group_chat_id_str = os.getenv("GROUP_CHAT_ID")
+if group_chat_id_str and group_chat_id_str.strip().lower() not in ("", "none"):
+    GROUP_CHAT_ID = int(group_chat_id_str)
 else:
     GROUP_CHAT_ID = None
+
 TIMEZONE = os.getenv("MOSCOW_TIMEZONE", "Europe/Moscow")
 MORNING_HOUR = int(os.getenv("MORNING_HOUR", 9))
 MORNING_MINUTE = int(os.getenv("MORNING_MINUTE", 0))
